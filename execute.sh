aa267ca870d02d62100e1765f8cdafceb5f23bfc
Pranav Varmaraja <pranavvarmaraja@gmail.com>
2022-10-03 10:42:56 -0700

    update execute.sh to compile then runthe network

--- execute.sh
+++ execute.sh
@@ -1,1 +1,9 @@
+if [ -d "./bin" ] 
+then
+    echo "Directory ./bin already exists." 
+else
+    mkdir bin
+fi
+javac -cp ./lib/json-simple-1.1.1.jar -s ./bin/Network ./src/Network.java -Xlint:unchecked
+mv ./src/*.class ./bin
 java -cp ./bin:./lib/json-simple-1.1.1.jar Network $1
